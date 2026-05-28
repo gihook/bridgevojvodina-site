ba52e64777926dc2b0bb3a9e97283e7b6f7676d1
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2026-05-28 15:37:00 +0200

    refactoring: updated application to read credentials from env variables

--- html/admin/connectdb.php
+++ html/admin/connectdb.php
@@ -1,8 +1,13 @@
 <?php 
 
-$conn = mysqli_connect('db', 'root', 'root_password', 'bridgevo_db');
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (mysqli_connect_errno()) {
-	die();
+	die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
